88a254fe42fa1bbc53f8f8e5a56c9e1ca49da326
athornburg <alexthornburg1@gmail.com>
2013-10-31 18:26:45 -0500

    Uncomment sql procedures

--- include/dbconnopen.php
+++ include/dbconnopen.php
@@ -1,5 +1,5 @@
 <?php
 //production database connection
-$cnnCDD = mysqli_connect("localhost:3306","root","QWEasdzx00")
+$cnnCDD = mysqli_connect("localhost:3306","root","password","datadictionary")
                     or die ("Error Connecting To The Database Because: " . mysqli_connect_error());
 ?>
