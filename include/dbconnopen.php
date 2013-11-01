25b0867fcb577d0150d6a7d35e9ab4d5c5abf9c6
athornburg <alexthornburg1@gmail.com>
2013-11-01 17:13:54 -0500

    Change to old example dbconnopen

--- include/dbconnopen.php
+++ include/dbconnopen.php
@@ -1,5 +1,5 @@
 <?php
 //production database connection
-$cnnCDD = mysqli_connect("localhost:3306","root","QWEasdzx00","datadictionary")
+$cnnCDD = mysqli_connect("yourlocaldatabase", "db-user", "db-password", "datadictionary")
                     or die ("Error Connecting To The Database Because: " . mysqli_connect_error());
 ?>
